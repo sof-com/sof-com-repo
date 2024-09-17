223c3238d978ec3192a5aa284fdc54825a77d851
davidmeltzer <david.meltzer@upm.es>
2024-09-17 16:18:38 +0200

    Versión para iniciar práctica 1.

---
+++ p1-calculadora/src/main/java/calculadora/AdaptadorOperacionesCalculadoraGUI.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+import CalculadoraGUI.ICalculadora;
+
+/** Esta clase sirve para adaptar la interfaz de la clase calculadora.OperacionesCalculadora a la interfaz
+ * de CalculadoraGUI.ICalculadora.
+ * Se puede utilizar un adaptador de clase o de objeto.
+ */
+public class AdaptadorOperacionesCalculadoraGUI implements ICalculadora {
+
+	// Escribir los métodos.
+
+}
---
+++ p1-calculadora/src/main/java/calculadora/AplicacionCalculadoraGUI.java
@@ -0,0 +1,18 @@
+package calculadora;
+
+/** Esta es la clase que contiene el método main() de la aplicación de la calculadora y de la aplicación cleinte de la calculadora.
+ * 
+ */
+public class AplicacionCalculadoraGUI {
+
+	/** Este es el método inicial de la aplicación. En él basta con instanciar CalculadoraGUI
+	 * pasándole una instancia de una clase que implementa OperacionesCalculadoraGUI.
+	 * @param args Argumentos del método main()
+	 */
+	public static void main(String[] args) {
+		
+		// Instanciar los objetos necesarios.
+		
+	}
+
+}
