8b39fd749946b0d21237dc3b7388021e37047711
davidmeltzer <david.meltzer@upm.es>
2024-11-28 01:06:50 +0100

    Versión para iniciar práctica 4.

---
+++ p4-servidor/src/main/java/servidor/AdaptadorOperacionesCalculadoraREST.java
@@ -0,0 +1,9 @@
+package servidor;
+
+/**
+ * Esta clase es el adaptador de OperacionesCalculadora.
+ * Expone un API para su uso de acuerdo a las especificaciones de la práctica.
+ */
+public class AdaptadorOperacionesCalculadoraREST {
+
+}
